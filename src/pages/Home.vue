7c31edb7ac56a2fc83defa8ba73208f3d6f2f4df
Yujin1219 <2271447@hansung.ac.kr>
2024-09-25 10:09:21 +0900

    Feat : 메인페이지 임시 구현

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,204 @@
 <template>
-    메인 페이지!!
+    <div class="container">
+        <div class="row mt-8">
+            <!--게시글 피드-->
+            <PostView />
+            <div class="col-lg-4">
+
+                <div class="card mb-5 mb-xl-8" style="background-color: #F1F7FF;">
+                    <!--begin::Header-->
+                    <div class="card-header border-0 pt-5">
+                        <h1 class="mt-4" style="text-shadow: 3px;">이달의 TOP 랭킹</h1>
+                        <div v-for="(category, index) in rankings" :key="index">
+                            <div class="mt-2">
+                                <h2 class="ms-2 fs-1">
+                                    <!--begin::Svg Icon | path: /var/www/preview.keenthemes.com/keenthemes/craft/docs/core/html/src/media/icons/duotune/general/gen020.svg-->
+                                    <span class="svg-icon svg-icon-warning svg-icon-2hx me-2"><svg width="26"
+                                            height="26" viewBox="0 0 24 24" fill="none"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M14 18V16H10V18L9 20H15L14 18Z" fill="currentColor" />
+                                            <path opacity="0.3"
+                                                d="M20 4H17V3C17 2.4 16.6 2 16 2H8C7.4 2 7 2.4 7 3V4H4C3.4 4 3 4.4 3 5V9C3 11.2 4.8 13 7 13C8.2 14.2 8.8 14.8 10 16H14C15.2 14.8 15.8 14.2 17 13C19.2 13 21 11.2 21 9V5C21 4.4 20.6 4 20 4ZM5 9V6H7V11C5.9 11 5 10.1 5 9ZM19 9C19 10.1 18.1 11 17 11V6H19V9ZM17 21V22H7V21C7 20.4 7.4 20 8 20H16C16.6 20 17 20.4 17 21ZM10 9C9.4 9 9 8.6 9 8V5C9 4.4 9.4 4 10 4C10.6 4 11 4.4 11 5V8C11 8.6 10.6 9 10 9ZM10 13C9.4 13 9 12.6 9 12V11C9 10.4 9.4 10 10 10C10.6 10 11 10.4 11 11V12C11 12.6 10.6 13 10 13Z"
+                                                fill="currentColor" />
+                                        </svg>
+                                    </span>
+                                    <span class="fs-2">{{ category.title }}</span>
+                                    
+                                </h2>
+                                <ul>
+                                    <li v-for="(item, idx) in category.items" :key="idx">
+                                        <span class="ms-2 me-5 fw-bold fs-1" style="font-variant-numeric: tabular-nums;">{{ idx + 1 }}</span>
+
+                                        <!-- 유저 아바타 -->
+                                        <div class="symbol symbol-40px me-5">
+                                            <img :src="`${item.profileImage}`" class="h-50 align-self-center" alt="" />
+                                        </div>
+                                        <!-- 유저 이름 및 회사 정보 -->
+                                        <!-- <div class="d-flex align-items-center flex-row-fluid flex-wrap"> -->
+                                        <div class="flex-grow-1 me-5">
+                                            <a class="text-gray-800 text-hover-primary fs-6 fw-bold">
+                                                {{ item.name }}
+                                            </a>
+
+                                        </div>
+                                        <span class="ms-10 ps-7">{{ item.amount }}</span>
+                                    </li>
+                                </ul>
+                                <br>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!--추천 친구-->
+                <div class="card mb-5 mb-xl-8">
+                    <!--begin::Header-->
+                    <div class="card-header border-0 pt-5">
+                        <h3 class="card-title align-items-start flex-column">
+                            <span class="d-flex align-items-center card-label fw-bold text-gray-900 fs-4">
+                                <i class="ki-duotone ki-people fs-2hx me-2">
+                                    <span class="path1"></span>
+                                    <span class="path2"></span>
+                                    <span class="path3"></span>
+                                    <span class="path4"></span>
+                                    <span class="path5"></span>
+                                </i>
+                                회원님을 위한 추천
+                            </span>
+
+                            <span class="text-muted mt-1 fw-bold fs-7 ms-11">나와 비슷한 소비 습관을 가진 사용자</span>
+                        </h3>
+
+                    </div>
+                    <!--end::Header-->
+                    <!--begin::Body-->
+                    <div class="card-body pt-5">
+                        <!-- User List 반복 -->
+                        <div v-for="(user, index) in users" :key="index">
+                            <!-- 유저 정보 -->
+                            <div class="d-flex flex-stack">
+                                <!-- 유저 아바타 -->
+                                <div class="symbol symbol-40px me-5">
+                                    <img :src="`${user.avatar}`" class="h-50 align-self-center" alt="" />
+                                </div>
+                                <!-- 유저 이름 및 회사 정보 -->
+                                <!-- <div class="d-flex align-items-center flex-row-fluid flex-wrap"> -->
+                                <div class="flex-grow-1 me-5">
+                                    <a :href="user.profileLink" class="text-gray-800 text-hover-primary fs-6 fw-bold">
+                                        {{ user.name }}
+                                    </a>
+                                    <span class="text-muted fw-semibold d-block fs-7">{{ user.company }}</span>
+                                </div>
+                                <a :href="user.profileLink" class="btn btn-sm btn-light fs-8 fw-bold">Follow</a>
+                                <!-- </div> -->
+                            </div>
+                            <div v-if="index !== users.length - 1" class="separator separator-dashed my-4"></div>
+                        </div>
+                    </div>
+                    <!--end::Body-->
+                </div>
+                <!--end::Social widget 1-->
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup>
+import PostView from '@/components/common/PostView.vue';
+import { ref } from 'vue';
+
+const rankings = ref([
+    {
+        title: '소비 TOP 3',
+        items: [
+            { name: 'Yujin_1219', amount: '1,000,000원', profileImage: 'src/assets/media/avatars/300-11.jpg' },
+            { name: 'Yujin_1219', amount: '900,000원', profileImage: 'src/assets/media/avatars/300-12.jpg' },
+            { name: 'Yujin_1219', amount: '600,000원', profileImage: 'src/assets/media/avatars/300-16.jpg' },
+        ],
+    },
+    {
+        title: '조회수 TOP 3',
+        items: [
+            { name: 'Yujin_1219', amount: '1,000,000회', profileImage: 'src/assets/media/avatars/300-4.jpg' },
+            { name: 'Yujin_1219', amount: '900,000회', profileImage: 'src/assets/media/avatars/300-2.jpg' },
+            { name: 'Yujin_1219', amount: '100,000회', profileImage: 'src/assets/media/avatars/300-6.jpg' },
+        ],
+    },
+]);
+
+const users = [
+    {
+        name: "Jacob Jones",
+        avatar: 'src/assets/media/avatars/300-11.jpg',
+        company: "Barone LLC.",
+        profileLink: "pages/user-profile/overview.html"
+    },
+    {
+        name: "Annette Black",
+        avatar: "src/assets/media/avatars/300-2.jpg",
+        company: "Binford Ltd.",
+        profileLink: "pages/user-profile/overview.html"
+    },
+    {
+        name: "Devon Lane",
+        avatar: "src/assets/media/avatars/300-7.jpg",
+        company: "Acme Co.",
+        profileLink: "pages/user-profile/overview.html"
+    },
+    {
+        name: "Kristin Watson",
+        avatar: "src/assets/media/avatars/300-9.jpg",
+        company: "Biffco Enterprises Ltd.",
+        profileLink: "pages/user-profile/overview.html"
+    },
+    {
+        name: "Eleanor Pena",
+        avatar: "src/assets/media/avatars/300-12.jpg",
+        company: "Abstergo Ltd.",
+        profileLink: "pages/user-profile/overview.html"
+    }
+];
 
 </script>
+
+<style scoped>
+.container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    background-color: #ffffff;
+    border-radius: 10px;
+    padding: 20px;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+}
+
+h1 {
+    text-align: center;
+    font-weight: bold;
+    margin-bottom: 20px;
+    width: 100%;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: flex;
+    align-items: center;
+    /* justify-content: space-between; */
+    padding: 8px;
+}
+
+li:last-child {
+    border-bottom: none;
+}
+
+
+img {
+    border-radius: 50%;
+    width: 30px;
+    height: 30px;
+}
+</style>
