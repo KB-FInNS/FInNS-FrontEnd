9001fc26d2ca4596cc2b2378085f6173e6a6d137
ingyeom <wjd1836@gmail.com>
2024-10-14 16:06:29 +0900

    Feat: 유저 검색 기능, 최근에 검색한 유저 기능 구현

--- src/components/common/SearchUser.vue
+++ src/components/common/SearchUser.vue
@@ -5,8 +5,8 @@
       <div style="position: relative; display: inline-block; width: 1000px;">
         <input
           v-model="searchQuery"
-          @keydown.enter="searchUsers"
-          @click="isDropdownVisible = true"
+          @keydown.enter="searchUsers()"
+          @click="clickSearchBar()"
           type="text"
           placeholder="유저 이름을 검색하세요"
           class="form-control dropbtn"
@@ -17,7 +17,7 @@
         <span
           class="menu-icon svg-icon svg-icon-2hx"
           style="position: absolute; right: 51%; top: 50%; transform: translateY(-50%); color: grey;">
-          <svg @click="searchUsers" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg @click="searchUsers()" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <rect opacity="0.5" x="17.0365" y="15.1223" width="8.15546" height="2" rx="1" transform="rotate(45 17.0365 15.1223)" fill="currentColor"/>
             <path d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z" fill="currentColor"/>
           </svg>
@@ -28,13 +28,14 @@
     <!-- 검색 결과 -->
     <div class="search-container dropdown-content" v-show="isDropdownVisible">
       <div v-if="filteredUsers.length" class="ms-2 me-2">
-        <ul class="list-group" style="max-height: 200px; overflow-y: auto;">
+        <ul class="list-group">
+          <h3 class="mt-5 mb-5" style="color: #7B7777;">검색된 유저</h3>
           <!-- 여러 개의 검색 결과를 출력 -->
-          <li v-for="user in filteredUsers" :key="user.id" class="list-group-item">
+          <li v-for="user in filteredUsers" :key="user.userNo" class="list-group-item">
             <div class="symbol symbol-40px">
               <img :src="`${user.imgUrl}`" />
             </div>
-            <a :href="`profile/${user.userNo}/spending`" class="fs-5 fw-bold text-gray-900 text-hover-primary mb-1">{{ user.userName }}</a>
+            <div @click="insertRecentUser(user.userNo)" class="fs-5 fw-bold text-gray-900 text-hover-primary mb-1">{{ user.userName }}</div>
             <button class="btn btn-sm" :class="user.isFollowing ? 'btn-primary' : 'btn-light'" @click="toggleFollow(user)">
               <span>{{ user.isFollowing ? '팔로잉' : '팔로우' }}</span>
             </button>
@@ -42,17 +43,15 @@
         </ul>
       </div>
       <div v-else class="mt-5 ms-2 me-2">
-        <h5 class="mb-7">검색 결과가 없습니다.</h5>
-        <h3 style="color: #7B7777;">최근 검색된 유저</h3>
-        <ul class="list-group mt-4">
-          <li v-for="user in recentUsers" :key="user.id" class="list-group-item">
-            <div class="symbol symbol-35px symbol-circle">
-              <span class="symbol-label bg-light-danger text-danger fw-semibold">{{ user.initial }}</span>
+        <ul class="list-group">
+          <h5 class="mt-5 mb-7 text-center" style="color:crimson;">검색 결과가 없습니다.</h5>
+          <h3 class="mb-5" style="color: #7B7777;">최근 검색된 유저</h3>
+          <!-- 여러 개의 검색 결과를 출력 -->
+          <li v-for="user in recentUsers" :key="user.userNo" class="list-group-item">
+            <div class="symbol symbol-40px">
+              <img :src="`${user.imgUrl}`" />
             </div>
-            <div class="ms-5">
-              <a href="profile/spending" class="fs-5 fw-bold text-gray-900 text-hover-primary mb-2">{{ user.userName }}</a>
-              <div class="fw-semibold text-muted">{{ user.email }}</div>
-            </div>
+            <div @click="insertRecentUser(user.userNo)" class="fs-5 fw-bold text-gray-900 text-hover-primary mb-1">{{ user.userName }}</div>
             <button class="btn btn-sm" :class="user.isFollowing ? 'btn-primary' : 'btn-light'" @click="toggleFollow(user)">
               <span>{{ user.isFollowing ? '팔로잉' : '팔로우' }}</span>
             </button>
@@ -66,6 +65,9 @@
 <script setup>
 import { ref, onMounted, onBeforeUnmount } from 'vue';
 import axios from 'axios';
+import router from '@/router';
+
+const auth = JSON.parse(localStorage.getItem('auth'));
 
 const users = ref([]);
 const getUsers = async () => {
@@ -73,23 +75,34 @@ const getUsers = async () => {
         const response = await axios.get(`http://localhost:8080/users`);
         users.value = response.data;
     } catch (error) {
-        console.error('Error fetching users value:', error);
+        console.error('Error fetching getUsers value:', error);
     }
 };
 
-const recentUsers = ref([
-  { id: 1, userName: 'Melody Macy', initial: 'M', isFollowing: true },
-  { id: 2, userName: 'John Doe', initial: 'J', isFollowing: true },
-  { id: 3, userName: 'Alice Smith', initial: 'A', isFollowing: true },
-  { id: 4, userName: 'Bob Johnson', initial: 'B', isFollowing: true },
-]);
+const recentUsers = ref([]);
+const getRecentUsers = async () => {
+    try {
+        const response = await axios.get(`http://localhost:8080/users/${auth.user.user_no}/recentUser`);
+        recentUsers.value = response.data;
+    } catch (error) {
+        console.error('Error fetching getRecentUsers value:', error);
+    }
+};
 
 const searchQuery = ref('');
 const filteredUsers = ref([]);
 const isDropdownVisible = ref(false);
 
+// 검색창 클릭 시 함수
+const clickSearchBar = async () => {
+  await getRecentUsers();
+  await getUsers();
+
+  isDropdownVisible.value = true;
+};
+
 // 검색 실행 함수
-const searchUsers = () => {
+const searchUsers = async () => {
   const query = searchQuery.value.toLowerCase();
   if (!searchQuery.value.trim()) {
     filteredUsers.value = [];
@@ -97,6 +110,16 @@ const searchUsers = () => {
   }
 
   filteredUsers.value = users.value.filter(user => user.userName.toLowerCase().includes(query));
+};
+
+
+const insertRecentUser = async (userNo) => {
+  try {
+      const response = await axios.put(`http://localhost:8080/recentUser/${auth.user.user_no}/${userNo}`);
+      router.push(`/profile/${userNo}/spending`);
+  } catch (error) {
+      console.error('Error fetching insertRecentUser value:', error);
+  }
 };
 
 // 입력 창 외부 클릭 시 dropdown 숨기기
@@ -109,9 +132,8 @@ const handleClickOutside = (event) => {
 };
 
 // 컴포넌트가 마운트된 후 이벤트 리스너 등록
-onMounted(async () => {
+onMounted(() => {
   document.addEventListener('click', handleClickOutside);
-  await getUsers();
 });
 
 // 컴포넌트가 언마운트될 때 이벤트 리스너 해제
@@ -162,5 +184,10 @@ img {
   border-radius: 50%;
   width: 30px;
   height: 30px;
+}
+
+.list-group{
+  max-height: 400px;
+  overflow-y: auto;
 }
 </style>
