8634c83c7a40ade4a1a5471c2dda63a2a0e9b891
ingyeom <wjd1836@gmail.com>
2024-10-15 03:30:01 +0900

    Feat: 팔로우 팔로워 기능 구현

--- src/components/common/FollowButton.vue
+++ src/components/common/FollowButton.vue
@@ -1,8 +1,8 @@
 <template>
   <button
     class="btn btn-sm ms-10"
-    :class="isFollowing ? 'btn-primary' : 'btn-light'"
-    @click="handleFollowClick"
+    :class="isFollowing ? 'btn-light' : 'btn-primary'"
+    @click="followClick"
   >
     <span>{{ isFollowing ? '팔로잉' : '팔로우' }}</span>
   </button>
@@ -23,55 +23,28 @@ const props = defineProps({
   },
 });
 
-const emit = defineEmits(['follow-status-changed']);
-
-// `initialIsFollowing`을 기준으로 반응형 변수 생성
 const isFollowing = ref(props.initialIsFollowing);
+const auth = JSON.parse(localStorage.getItem('auth'));
 
-// `initialIsFollowing`이 변경될 경우 `isFollowing` 동기화
-watch(
-  () => props.initialIsFollowing,
-  (newValue) => {
-    isFollowing.value = newValue;
-  }
-);
+// `props.initialIsFollowing`의 변화를 감지해서 `isFollowing`을 업데이트
+watch(() => props.initialIsFollowing, (newVal) => {
+  isFollowing.value = newVal;
+});
 
-const handleFollowClick = async () => {
+const followClick = async () => {
   try {
-    const authData = JSON.parse(localStorage.getItem('auth'));
-    const user_no = authData.user.user_no;
-
-    if (!authData || !authData.token) {
-      throw new Error('인증 정보가 유효하지 않습니다.');
-    }
-
     let response;
-
     if (isFollowing.value) {
       // 언팔로우 요청
-      response = await axios.delete(
-        `http://localhost:8080/follow/${props.to_user_no}`,
-        {
-          headers: { Authorization: `Bearer ${authData.token}` },
-          data: { user_no }, // 서버에 요청할 때 유저 번호 함께 보냄
-        }
-      );
+      response = await axios.delete(`http://localhost:8080/unfollow/${auth.user.user_no}/${props.to_user_no}`);
     } else {
       // 팔로우 요청
-      response = await axios.post(
-        `http://localhost:8080/follow/${props.to_user_no}`,
-        { user_no },
-        {
-          headers: { Authorization: `Bearer ${authData.token}` },
-        }
-      );
+      response = await axios.post(`http://localhost:8080/follow/${auth.user.user_no}/${props.to_user_no}`);
     }
 
     // 팔로우 상태 변경
     isFollowing.value = !isFollowing.value;
 
-    // 상태가 변경되면 부모 컴포넌트에 알림
-    emit('follow-status-changed', props.to_user_no, isFollowing.value);
   } catch (error) {
     console.error('팔로우/언팔로우 요청 실패:', error);
   }
